a6d3fa101ef9885d7b7d8489829de1603193b197
renedietzjs <kuchenmann.rene@gmail.com>
2022-01-06 19:18:03 +0100

    Added assets

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img :src="require('@/assets/logo_black.svg')" />
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
+
+<style scoped></style>
