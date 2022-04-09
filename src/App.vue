642d1d5d2bea9f294f999fea052a525debc77a4d
renedietzjs <kuchenmann.rene@gmail.com>
2022-04-09 16:36:21 +0200

    Added countdown page Code will be removed afterwards

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,71 @@
 <template>
   <div id="app">
-    <Navbar />
+    <!-- <Navbar />
     <router-view />
-    <Footer />
+    <Footer /> -->
+    <section class="hero is-fullheight has-background-bright-green">
+      <div
+        class="hero-body is-justify-content-center"
+        style="min-height: 100vh"
+      >
+        <div class="has-text-centered">
+          <img
+            class="mb-5"
+            :src="require('@/assets/logo_black.svg')"
+            width="256"
+          />
+          <h1 class="title is-1 amaranth">PhoGrow</h1>
+          <h3 class="subtitle is-3">Experience plant growth in 3D.</h3>
+          <p class="has-text-weight-semibold mb-6">
+            <span class="has-background-white py-2 px-4 is-rounded">
+              {{ countdown }} days
+            </span>
+          </p>
+          <span class="icon is-large has-text-grey">
+            <i class="material-icons-round is-size-1">keyboard_arrow_down</i>
+          </span>
+        </div>
+      </div>
+      <div class="hero-foot">
+        <figure class="image is-16by9">
+          <iframe
+            class="has-ratio br-2"
+            src="https://youtube.com/embed/8lLOavvdYIc"
+            allowfullscreen
+          ></iframe>
+        </figure>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue';
-import Footer from '@/components/Footer.vue';
+// import Navbar from '@/components/Navbar.vue';
+// import Footer from '@/components/Footer.vue';
+import countdown from 'countdown';
 
 export default {
   name: 'App',
   components: {
-    Navbar,
-    Footer
+    // Navbar,
+    // Footer
+  },
+  data() {
+    return {
+      countdown: 0
+    };
+  },
+  created() {
+    const timerId = countdown(
+      new Date(2022, 4, 2),
+      ({ days }) => {
+        this.countdown = days;
+        if (new Date().getDay() == new Date(2022, 4, 2).getDay()) {
+          window.clearInterval(timerId);
+        }
+      },
+      countdown.DAYS
+    );
   }
 };
 </script>
